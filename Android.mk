e120d9ab67458a76cb80f3f5a6c94aae4e88b215
manueljpy <manueljpy@gmail.com>
2019-04-13 17:26:08 +0200

    quitado parse del .mk

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sdcard.c
 LOCAL_MODULE := sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror -ggdb -O0 -DHAVE_LIBXML
-LOCAL_SHARED_LIBRARIES := libcutils libpackagelistparser libchall
+LOCAL_SHARED_LIBRARIES := libcutils libchall
 LOCAL_STRIP_MODULE := false
 
 include $(BUILD_EXECUTABLE)
